ef5e5a3776a76df3f9b3f26123737b82013a7336
George Khubua <george.khubua@live.com>
2016-03-29 19:36:18 +0300

    Add navigation menu

---
+++ WebUI/Controllers/DownloadsController.cs
@@ -0,0 +1,20 @@
+﻿using LOGA.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LOGA.WebUI.Controllers
+{
+    public class DownloadsController : Controller
+    {
+        
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
