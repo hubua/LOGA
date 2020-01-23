82409311510b0415ded14a623050f1a22e0ef0a8
George Khubua <george.khubua@live.com>
2020-01-23 18:23:13 +0200

    Fix incorrect route

--- BeboenaWebApp/Startup.cs
+++ BeboenaWebApp/Startup.cs
@@ -98,7 +98,9 @@ namespace BeboenaWebApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{lid?}");
             });
 
             var dirinfo = env.ContentRootFileProvider.GetFileInfo("/Services/Data");
